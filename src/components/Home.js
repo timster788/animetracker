e70c4fa977524023f7efbbad26ea9e0a43beeded
Timothy Dao <timster78811@gmail.com>
2018-09-10 16:01:15 -0400

    got front end to communicate with api

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import AnimeHome from './AnimeHome';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <AnimeHome />
+      </div>
+    );
+  }
+}
